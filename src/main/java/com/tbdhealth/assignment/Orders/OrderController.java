017cb3d94f31be6a7f31705f2cd23fde69893122
rohit <rohit.rew@gmail.com>
2023-05-03 14:33:33 +0530

    gitignore modified

--- src/main/java/com/tbdhealth/assignment/Orders/OrderController.java
+++ src/main/java/com/tbdhealth/assignment/Orders/OrderController.java
@@ -1,9 +1,5 @@
 package com.tbdhealth.assignment.Orders;
 
-import com.tbdhealth.assignment.Configuration;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
